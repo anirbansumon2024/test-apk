5e3dbddfdbd07cc2d91d6ba02ed1ad5c6de67768
anirban sumon <154916693+anirbansumon2024@users.noreply.github.com>
2026-05-01 22:50:41 +0600

    Add MainActivity class with onCreate method

--- app/src/main/java/com/example/empty/MainActivity.java
+++ app/src/main/java/com/example/empty/MainActivity.java
@@ -1,1 +1,13 @@
 
+package com.example.empty;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
